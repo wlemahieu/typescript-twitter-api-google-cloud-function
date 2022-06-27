e912e58eee7cb45238744fb1497531352e4c8d19
Wesley LeMahieu <lateralis3k@gmail.com>
2022-06-27 14:14:48 -0700

    use axios for direct twitter v2 api communication

--- src/index.ts
+++ src/index.ts
@@ -7,49 +7,66 @@
  * Example #2 lists all of your own Tweets using tail-call recursion
  */
 import type {HttpFunction} from '@google-cloud/functions-framework/build/src/functions';
-import {Client} from 'twitter-api-sdk';
+import axios from 'axios';
 
 const getTweets = async (
-  client: Client,
   myUserId: string,
   perPage: number,
   nextToken?: string
 ) => {
   let tweets: Array<any> = [];
-
-  const results = await client.tweets.usersIdTweets(myUserId, {
-    max_results: perPage,
-    pagination_token: nextToken,
+  let url = `https://api.twitter.com/2/users/${myUserId}/tweets?tweet.fields=entities&max_results=${perPage}`;
+  if (nextToken) {
+    url = `${url}&pagination_token=${nextToken}`;
+  }
+  console.log(url);
+  const results = await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+    },
   });
 
-  const data: any = results?.data;
+  const data: any = results?.data?.data || [];
   tweets = [...tweets, ...data];
 
+  /*
   if (results?.meta?.next_token?.length) {
     console.log('Getting more tweets!');
     await getTweets(client, myUserId, perPage, results.meta?.next_token);
   }
+  */
   return tweets;
 };
 
 export const run: HttpFunction = async (_req, res) => {
+  // allow our local apps to interact with this gcf. (affects local only)
+  res.set('Access-Control-Allow-Origin', '*');
+
   console.log('Connecting to Twitter API...');
 
-  const client = new Client(process.env.TWITTER_BEARER_TOKEN as string);
-
   // get the 20th ever tweet!
+  /*
   console.log('Fetching an old tweet by Jack...');
   const tweet = await client.tweets.findTweetById('20');
   console.log(tweet?.data?.text);
+  */
 
-  // get all my tweets and liked tweets
   try {
     console.log('Fetching all my tweets...');
-    const myUser = await client.users.findUserByUsername('WesleyLeMahieu');
+    const myUser = await axios.get(
+      'https://api.twitter.com/2/users/by/username/WesleyLeMahieu',
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+        },
+      }
+    );
 
-    if (myUser.data) {
+    const myUserId = myUser.data?.data?.id;
+
+    if (myUserId) {
       // const likes = await client.tweets.usersIdLikedTweets(myUser.data.id);
-      const tweets = await getTweets(client, myUser.data.id, 100);
+      const tweets = await getTweets(myUserId, 100);
       res.send({
         // likes: likes?.data,
         tweets,
